ae51ca2d42f0c0add7bdf25417b503c0bc1216d1
Daniel Ramirez <zoerunner@gmail.com>
2021-10-08 11:47:38 -0700

    Add framework for AI class

---
+++ ai.py
@@ -0,0 +1,8 @@
+from player import Player
+class AI(Player):
+    def __init__(self, name, gestures):
+        super().__init__()
+
+    def select_gesture(self):
+        # TODO automate selection
+        return super().select_gesture()
