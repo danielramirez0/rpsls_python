1589f34fadacfe7cd3b3c5a05aba7197e221d9cd
Ben Bissett <benjaminbissett@gmail.com>
2021-10-08 17:15:07 -0400

    Good Working State

--- game.py
+++ game.py
@@ -24,7 +24,7 @@ class Game:
         print('Welcome to Rock Paper Scissor Lizard Spock!')
 
     def display_winner(self):
-        pass
+        print(f'{self.winner} won!!!')
 
     def display_rules(self):
         print('The game RPSLS is just like Rock Paper Scissors, however there are now five options to choose from!\nRules:\nRock beats Scissor and Lizard\nPaper beats Rock and Spock\nScissor beats Paper and Lizard\nLizard beats Spock and Paper\nSpock beats Scissors and Rock')
@@ -35,7 +35,7 @@ class Game:
             self.ai = False
 
     def round_count(self):
-        self.rounds = self.prompt('How many rounds would you like to play?\n', self.validator.is_odd, False)
+        self.rounds = int(self.prompt('How many rounds would you like to play?\n', self.validator.is_odd, False))
 
     def start(self):
         player_one = Human(self.prompt('Player 1, enter your name: ', self.validator.auto_valid, False))
@@ -45,5 +45,21 @@ class Game:
             player_two = Human('Player 2, enter your name: ', self.validator.auto_valid, False)
         
         while True:
-            print(f"{player_one.select_gesture().name}")
-            print(f'{player_two.select_gesture().name}')
+            player_one_choice = player_one.select_gesture()
+            player_two_choice = player_two.select_gesture()
+            print(f"{player_one.name} chose {player_one_choice.name}")
+            print(f'{player_two.name} chose {player_two_choice.name}')
+            if player_one_choice.name == player_two_choice.name:
+                print('Tie!')
+            elif player_one_choice.name in player_two_choice.weaknesses:
+                print(f'{player_one.name} wins this round')
+                self.player_one_score += 1
+            elif player_one_choice.name in player_two_choice.strengths:
+                print(f'{player_two.name} wins this round')
+                self.player_two_score += 1
+            if self.player_one_score / self.rounds > .5:
+                self.winner = player_one.name
+                break
+            elif self.player_two_score / self.rounds > .5:
+                self.winner = player_two.name
+                break
